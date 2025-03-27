b6945449790349d5c383de05c45c336fe4895e1f
Tibor Felföldy <tiboofficial@gmail.com>
2025-03-27 23:45:13 +0100

    First try of external release

--- Package.swift
+++ Package.swift
@@ -3,18 +3,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "UniversalSceneRuntime",
+    name: "SceneXCore",
     platforms: [.iOS(.v18), .macOS(.v15), .visionOS(.v2)],
     products: [
         .library(
-            name: "UniversalSceneRuntime",
-            targets: ["UniversalSceneRuntime"]
+            name: "SceneXCore",
+            targets: ["SceneXCore"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "UniversalSceneRuntime",
-            path: "./UniversalSceneRuntime.xcframework"
+            name: "SceneXCore",
+            url: "https://github.com/scenex-engine/scenex-core/releases/download/v0.1.0/UniversalSceneRuntime.xcframework.zip",
+            checksum: "6cee2dcc21e5ac2b7a948f76dead405abda8f887994c016d55daac24dc64f2ac"
         )
     ]
 )
