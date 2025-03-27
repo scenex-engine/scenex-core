6f43e9aa2fdd76020058044ec48b5f307cfffda8
Tibor Felföldy <tiboofficial@gmail.com>
2025-03-27 23:11:00 +0100

    Add simulator

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,10 +8,13 @@ let package = Package(
     products: [
         .library(
             name: "UniversalSceneRuntime",
-            targets: ["UniversalSceneRuntimeBin"]
+            targets: ["UniversalSceneRuntime"]
         ),
     ],
     targets: [
-        .binaryTarget(name: "UniversalSceneRuntimeBin", path: "./UniversalSceneRuntime.xcframework")
+        .binaryTarget(
+            name: "UniversalSceneRuntime",
+            path: "./UniversalSceneRuntime.xcframework"
+        )
     ]
 )
---
+++ UniversalSceneRuntime.xcframework/ios-arm64_x86_64-simulator/UniversalSceneRuntime.framework/Headers/UniversalSceneRuntime.h
@@ -0,0 +1,18 @@
+//
+//  UniversalSceneRuntime.h
+//  UniversalSceneRuntime
+//
+//  Created by Tibor Felföldy on 2025-03-26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UniversalSceneRuntime.
+FOUNDATION_EXPORT double UniversalSceneRuntimeVersionNumber;
+
+//! Project version string for UniversalSceneRuntime.
+FOUNDATION_EXPORT const unsigned char UniversalSceneRuntimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniversalSceneRuntime/PublicHeader.h>
+
+
